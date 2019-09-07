c98fe654edc7aa3ddc3e5939677e47a0b67cacce
tuanld <tuan.ld@isofh.com>
2019-09-07 10:56:46 +0700

    update

--- src/main/java/com/github/ledangtuanbk/GsonUtil.java
+++ src/main/java/com/github/ledangtuanbk/GsonUtil.java
@@ -1,10 +1,9 @@
 package com.github.ledangtuanbk;
 
-import site.freeappvn.test.MyLib;
 
 public class GsonUtil {
     public static int plus(int a, int b){
-        return MyLib.plus(a,b);
+        return a+b+b;
     }
 
 }
