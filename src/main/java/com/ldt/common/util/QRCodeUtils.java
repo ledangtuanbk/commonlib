bcd85546defe0aaa68c4a897965799a50d16a1da
tuanld <tuan.ld@isofh.com>
2019-09-10 18:00:43 +0700

    qr code utils

---
+++ src/main/java/com/ldt/common/util/QRCodeUtils.java
@@ -0,0 +1,21 @@
+package com.ldt.common.util;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class QRCodeUtils {
+    public static byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "JPEG", pngOutputStream);
+        byte[] pngData = pngOutputStream.toByteArray();
+        return pngData;
+    }
+}
---
+++ src/main/java/test/util/ArrayUtilsTest.java
@@ -0,0 +1,23 @@
+package test.util;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayUtilsTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        System.out.println("setup ");
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+        System.out.println("tearDown ");
+    }
+
+    @Test
+    void concatIntegerWithComma() {
+    }
+}
